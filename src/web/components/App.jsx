9e368860cbce2d1e8ea10719ce9f70452d9a4d0d
naveedaheer <naveedaheer@gmail.com>
2017-05-26 12:28:38 +0500

    albumlist added

--- src/web/components/App.jsx
+++ src/web/components/App.jsx
@@ -4,11 +4,15 @@
 */
 import React from 'react';
 import Header from './header';
+import AlbumList from './AlbumList';
 
 export default class App extends React.Component {
   render() {
     return (
+      <div>
      <Header headerText={'Albums!'} />
+     <AlbumList />
+     </div>
     );
   }
 }
